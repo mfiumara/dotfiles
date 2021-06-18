1ab0a22fd8ddd607fc08e5788bca2e07b42eb10d
Mattia Fiumara <mattiafiumara@gmail.com>
2021-06-18 20:04:37 +0200

    :add ohmyzsh

--- install.sh
+++ install.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 echo "Installing VIM Plugin manager"
-
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 echo "Copying .vimrc and .zshrc into home folder"
 cp .vimrc ~
 cp .zshrc ~
 
+echo "Installing ohmyzsh"
+sudo pacman -S zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
