048123801bc565607ecb1c1ebb3ea132305cece0
Mattia Fiumara <mattia@agurotech.com>
2025-01-21 17:07:13 +0100

    wip: add file structure from https://lazy.folke.io/usage/structuring

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,23 +1,2 @@
--- Lazypath plugin manager
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-
--- Load global settings
-require("settings")
-
--- Require lazy to load plugins
-require("lazy").setup("plugins")
-
--- Load color scheme
-require("colorscheme")
-
+-- Load lazy plugin manager
+require("config.lazy")
