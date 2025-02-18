2252efe77ba57099c3b106bbf39fe688b34bf358
Mattia Fiumara <mattia@agurotech.com>
2025-02-18 09:03:11 +0100

    maint: add treesitter

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,27 +1,33 @@
 -- plugins/telescope.lua:
 return {
-	"nvim-telescope/telescope.nvim",
-	tag = "0.1.8",
-	dependencies = { "nvim-lua/plenary.nvim" },
-	init = function()
-		require("telescope").setup({
-			defaults = {
-				layout_strategy = "vertical",
-			},
-		})
-		-- Define Key Mappings
-		local map = vim.api.nvim_set_keymap
-		local opts = { noremap = true, silent = true }
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.8",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		init = function()
+			require("telescope").setup({
+				defaults = {
+					layout_strategy = "vertical",
+				},
+			})
+			-- Define Key Mappings
+			local map = vim.api.nvim_set_keymap
+			local opts = { noremap = true, silent = true }
 
-		map("n", "<leader>bb", "<cmd>Telescope buffers<CR>", opts)
-		map("n", "<leader>?", "<cmd>TodoTelescope<CR>", opts)
-		-- Respects .gitignore
-		map("n", "<C-p>", "<cmd>Telescope find_files<CR>", opts)
-		-- Find all files and ignore .gitignore
-		map("n", "<leader>fa", "<cmd>Telescope find_files no_ignore=true<CR>", opts)
-		map("n", "<leader>ff", "<cmd>Telescope live_grep<CR>", opts)
-		map("n", "<leader>fb", "<cmd>Telescope buffers<CR>", opts)
-		map("n", "<leader>fh", "<cmd>Telescope help_tags<CR>", opts)
-		map("n", "<leader>fs", "<cmd>Telescope grep_string<CR>", opts)
-	end,
+			map("n", "<leader>bb", "<cmd>Telescope buffers<CR>", opts)
+			map("n", "<leader>?", "<cmd>TodoTelescope<CR>", opts)
+			-- Respects .gitignore
+			map("n", "<C-p>", "<cmd>Telescope find_files<CR>", opts)
+			-- Find all files and ignore .gitignore
+			map("n", "<leader>fa", "<cmd>Telescope find_files no_ignore=true<CR>", opts)
+			map("n", "<leader>ff", "<cmd>Telescope live_grep<CR>", opts)
+			map("n", "<leader>fb", "<cmd>Telescope buffers<CR>", opts)
+			map("n", "<leader>fh", "<cmd>Telescope help_tags<CR>", opts)
+			map("n", "<leader>fs", "<cmd>Telescope grep_string<CR>", opts)
+		end,
+	},
+	{
+		"nvim-telescope/telescope-fzf-native.nvim",
+		build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release",
+	},
 }
--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,14 +1,14 @@
 return {
 	-- Upgrade to nvim 0.10.0 before using treesitter
-	--  {
-	--    "nvim-treesitter/nvim-treesitter",
-	--    build = ":TSUpdate", -- Automatically update parsers after installation
-	--    init = function()
-	--      require("nvim-treesitter.configs").setup {
-	--        ensure_installed = { "c", "lua", "rust" }, -- Languages to install
-	--        highlight = { enable = true }, -- Enable syntax highlighting
-	--        indent = { enable = true }, -- Enable indentation
-	--      }
-	--    end,
-	--  },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate", -- Automatically update parsers after installation
+		init = function()
+			require("nvim-treesitter.configs").setup({
+				ensure_installed = { "c", "lua", "rust" }, -- Languages to install
+				highlight = { enable = true }, -- Enable syntax highlighting
+				indent = { enable = true }, -- Enable indentation
+			})
+		end,
+	},
 }
