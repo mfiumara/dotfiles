69f3b141081010968a61b0e0661de8adfa511278
Mattia Fiumara <mattia@agurotech.com>
2025-02-20 11:31:35 +0100

    fix: broken telescope

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -11,27 +11,27 @@ return {
 			},
 			{ 'nvim-tree/nvim-web-devicons',            enabled = true },
 			{ 'nvim-telescope/telescope-ui-select.nvim' },
-			init = function()
-				require("telescope").setup({
-					defaults = {
-						layout_strategy = "vertical",
-					},
-				})
-				-- Define Key Mappings
-				local map = vim.api.nvim_set_keymap
-				local opts = { noremap = true, silent = true }
-
-				map("n", "<leader>bb", "<cmd>Telescope buffers<CR>", opts)
-				map("n", "<leader>?", "<cmd>TodoTelescope<CR>", opts)
-				-- Respects .gitignore
-				map("n", "<C-p>", "<cmd>Telescope find_files<CR>", opts)
-				-- Find all files and ignore .gitignore
-				map("n", "<leader>fa", "<cmd>Telescope find_files no_ignore=true<CR>", opts)
-				map("n", "<leader>ff", "<cmd>Telescope live_grep<CR>", opts)
-				map("n", "<leader>fb", "<cmd>Telescope buffers<CR>", opts)
-				map("n", "<leader>fh", "<cmd>Telescope help_tags<CR>", opts)
-				map("n", "<leader>fs", "<cmd>Telescope grep_string<CR>", opts)
-			end,
 		},
-	}
+		init = function()
+			require("telescope").setup({
+				defaults = {
+					layout_strategy = "vertical",
+				},
+			})
+			-- Define Key Mappings
+			local map = vim.api.nvim_set_keymap
+			local opts = { noremap = true, silent = true }
+
+			map("n", "<leader>bb", "<cmd>Telescope buffers<CR>", opts)
+			map("n", "<leader>?", "<cmd>TodoTelescope<CR>", opts)
+			-- Respects .gitignore
+			map("n", "<C-p>", "<cmd>Telescope find_files<CR>", opts)
+			-- Find all files and ignore .gitignore
+			map("n", "<leader>fa", "<cmd>Telescope find_files no_ignore=true<CR>", opts)
+			map("n", "<leader>ff", "<cmd>Telescope live_grep<CR>", opts)
+			map("n", "<leader>fb", "<cmd>Telescope buffers<CR>", opts)
+			map("n", "<leader>fh", "<cmd>Telescope help_tags<CR>", opts)
+			map("n", "<leader>fs", "<cmd>Telescope grep_string<CR>", opts)
+		end,
+	},
 }
