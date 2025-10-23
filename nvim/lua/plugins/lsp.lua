2ed63c326a2945cbc798b7e4c66c0c28b3a953ba
Mattia Fiumara <mattia@agurotech.com>
2025-10-23 09:20:01 +0200

    maint: remove copilot, update plugins

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -229,7 +229,7 @@ return
 		vim.list_extend(ensure_installed, {
 			"stylua", -- Used to format Lua code
 		})
-		-- require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
+		require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
 		-- require("mason-lspconfig").setup({
 		-- 	handlers = {
